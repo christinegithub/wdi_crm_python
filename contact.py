85b9991ae01e9426dec50a33842a261f78b1b612
christinegithub <christine13@gmail.com>
2019-02-25 14:42:39 -0500

    exercise complete

--- contact.py
+++ contact.py
@@ -28,45 +28,111 @@ class Contact:
     def all(cls):
         """This method should return all of the existing contacts"""
 
-    @classmethod
-    def find(cls):
-        """ This method should accept an id as an argument
-        and return the contact who has that id
-        """
+        for contact in cls.contacts:
+            print(contact.first_name, contact.last_name, contact.email,
+                  contact.note)
 
-    def update(self):
+    @classmethod
+    def find(cls, id_number):
+        """ This method should accept an id as an argument
+        and return the contact who has that id"""
+
+        for contact in cls.contacts:
+            if contact.id == id_number:
+                return contact
+        return False
+
+    def update(self, attribute, value):
         """ This method should allow you to specify
         1. which of the contact's attributes you want to update
         2. the new value for that attribute
-        and then make the appropriate change to the contact
-        """
+        and then make the appropriate change to the contact"""
+
+        if attribute == "first_name":
+            self.first_name = value
+        elif attribute == "last_name":
+            self.last_name = value
+        elif attribute == "email":
+            self.email = value
+        elif attribute == "note":
+            self.note = value
+        else:
+            print("That field does not exist")
+            return False
+
+        print("You have updated '{}' to '{}'".format(attribute, value))
+        print(self.first_name, self.last_name, self.email, self.note)
 
     @classmethod
-    def find_by(cls):
+    def find_by(cls, attribute, value):
         """This method should work similarly to the find method above
         but it should allow you to search for a contact using attributes other
         than id by specifying both the name of the attribute and the value
         eg. searching for 'first_name', 'Betty' should return the first contact
         named Betty"""
 
+        for contact in cls.contacts:
+            if attribute == "first_name" and contact.first_name == value:
+                print(contact.first_name, contact.last_name, contact.email,
+                      contact.note)
+                return True
+            elif attribute == "last_name" and contact.last_name == value:
+                print(contact.first_name, contact.last_name, contact.email,
+                      contact.note)
+                return True
+            elif attribute == "email" and contact.email == value:
+                print(contact.first_name, contact.last_name, contact.email,
+                      contact.note)
+                return True
+            elif attribute == "note" and contact.note == value:
+                print(contact.first_name, contact.last_name, contact.email,
+                      contact.note)
+                return True
+
+        print("That information does not exist.")
+        return False
+
     @classmethod
     def delete_all(cls):
         """This method should delete all of the contacts"""
 
+        del cls.contacts[:]
+
     def full_name(self):
         """Returns the full (first and last) name of the contact"""
+
+        print(self.first_name, self.last_name)
 
     def delete(self):
         """This method should delete the contact
         HINT: Check the Array class docs for built-in methods that might be
         useful here"""
 
+        Contact.contacts.remove(self)
 
-christine = Contact.create("christine", "lee", "chris@gmail.com", "new user")
-bill = Contact.create("bill", "gates", "bill@gmail.com", "second user")
-print(christine.first_name)
-print(bill.note)
-print(Contact.contacts)
-print(len(Contact.contacts))
-print(christine.id)
-print(bill.id)
+
+christine = Contact.create("Christine", "Lee", "chris@gmail.com", "new user")
+bill = Contact.create("Bill", "Nye", "bill@gmail.com", "second user")
+tom = Contact.create("Tom", "Jones", "tom@gmail.com", "third user")
+# print(christine.first_name)
+# print(bill.note)
+# print(Contact.contacts)
+# print(len(Contact.contacts))
+# print(christine.id)
+# print(bill.id)
+# Contact.all()
+# Contact.find(1)
+# Contact.find(3)
+# christine.update("note", "new note on file")
+# Contact.find_by("email", "chris@gmail.com")
+# Contact.find_by("email", "tom1@gmail.com")
+# Contact.delete_all()
+# print(Contact.contacts)
+# christine.full_name()
+# bill.full_name()
+# tom.full_name()
+# print(len(Contact.contacts))
+# bill.delete()
+# print(len(Contact.contacts))
+# print(Contact.contacts[0].first_name)
+# print(Contact.contacts[1].first_name)
--- crm.py
+++ crm.py
@@ -1,25 +1,92 @@
 from contact import Contact
+import sys
+
 
 class CRM:
 
-  # def main_menu(self):
-  #
-  #
-  # def print_main_menu(self):
-  #
-  #
-  # def call_option(self):
-  #
-  #
-  # def add_new_contact(self):
-  #
-  #
-  # def modify_existing_contact(self):
-  #
-  #
-  # def delete_contact(self):
-  #
-  #
-  # def display_all_contacts(self):
-  #
-  # def search_by_attribute(self):
+    def main_menu(self):
+        while True:
+            self.print_main_menu()
+            user_selected = int(input())
+            self.call_option(user_selected)
+
+    def print_main_menu(self):
+        print('[1] Add a new contact')
+        print('[2] Modify an existing contact')
+        print('[3] Delete a contact')
+        print('[4] Display all the contacts')
+        print('[5] Search by attribute')
+        print('[6] Exit')
+        print('Enter a number: ')
+
+    def call_option(self, user_selected):
+        if user_selected == 1:
+            self.add_new_contact()
+        elif user_selected == 2:
+            self.modify_existing_contact()
+        elif user_selected == 3:
+            self.delete_contact()
+        elif user_selected == 4:
+            self.display_all_contacts()
+        elif user_selected == 5:
+            self.search_by_attribute()
+        elif user_selected == 6:
+            sys.exit()
+
+    def add_new_contact(self):
+
+        print("Enter First Name: ")
+        first_name = input()
+
+        print("Enter Last Name: ")
+        last_name = input()
+
+        print("Enter Email Address: ")
+        email = input()
+
+        print("Enter a Note: ")
+        note = input()
+
+        Contact.create(first_name, last_name, email, note)
+
+    def modify_existing_contact(self):
+
+        print("What is the ID of the contact you would like to update?")
+        id_number = int(input())
+
+        print("What would you like to update? (first_name, last_name, email, note)")
+        attribute = input()
+
+        print("What would you like to change it to?")
+        value = input()
+
+        current_contact = Contact.find(id_number)
+
+        current_contact.update(attribute, value)
+
+    def delete_contact(self):
+
+        print("What is the ID of the contact you would like to delete?")
+        id_number = int(input())
+
+        current_contact = Contact.find(id_number)
+
+        current_contact.delete()
+
+    def display_all_contacts(self):
+
+        Contact.all()
+
+    def search_by_attribute(self):
+
+        print("Which field are you searching in? (first_name, last_name, email, note)")
+        attribute = input()
+
+        print("What information are you looking for? ")
+        value = input()
+
+        Contact.find_by(attribute, value)
+
+
+a_crm_app = CRM()
+a_crm_app.main_menu()
